9e8ac4f580a4e28d4e329f17b1bf66c1fd17c25b
lukec <lukec@e5f7e8ef-ac07-0410-9fef-9965b2c2b5fc>
2007-02-22 05:47:52 +0000

    add mocked docs
    git-svn-id: http://5thplane.com/svn/perl/mocked@202 e5f7e8ef-ac07-0410-9fef-9965b2c2b5fc

--- lib/mocked.pm
+++ lib/mocked.pm
@@ -2,21 +2,69 @@ package mocked;
 use strict;
 use warnings;
 
+=head1 NAME
+
+mocked - use mocked libraries in unit tests
+
+=head1 SYNOPSIS
+
+  # use a fake LWP::Simple for testing from t/lib/LWP/Simple.pm
+  use mocked 'LWP::Simple';
+  my $text = get($url);
+
+=head1 DESCRIPTION
+
+Often during unit testing, you may find the need to use mocked libraries
+to test edge cases, or prevent unit tests from using slow or external
+code.
+
+This is where mocking libraries can help.
+
+When you mock a library, you are creating a fake one that will be used
+in place of the real one.  The code can do as much or as little as is
+needed.
+
+Use mocked.pm as a safety measure (be sure you're actually using the
+mocked module), and as a way to document the tests for future
+maintainers.
+
+=cut
+
 our $VERSION = '0.01';
+
+=head1 FUNCTIONS
+
+=head2 import
+
+This function will make sure the module you specify is loaded from t/lib.
+
+=cut
 
 sub import {
     my $class = shift;
     my $module = shift;
 
     my $mock_path = 't/lib';
-    unshift @INC, $mock_path;
+    my @old_inc = @INC;
+    @INC = ($mock_path);
     eval "require $module";
-    shift @INC;
+    @INC = @old_inc;
     die if $@;
 
     my $import = $module->can('import');
     @_ = ($module, @_);
     goto &$import if $import;
 }
+
+=head1 AUTHOR
+
+Luke Closs, C<< <cpan at 5thplane.com> >>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
 
 1;
